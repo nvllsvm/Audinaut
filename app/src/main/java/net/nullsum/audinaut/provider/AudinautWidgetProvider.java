3648f64fa4e8ad79243d56fbdd1e9728e41ed6f5
Andrew Rabert <ar@nullsum.net>
2019-08-24 21:29:51 -0400

    Use vector images Thanks to vrih https://github.com/daneren2005/Subsonic/pull/919

--- app/src/main/java/net/nullsum/audinaut/provider/AudinautWidgetProvider.java
+++ app/src/main/java/net/nullsum/audinaut/provider/AudinautWidgetProvider.java
@@ -239,9 +239,9 @@ public class AudinautWidgetProvider extends AppWidgetProvider {
 
         // Set correct drawable for pause state
         if (playing) {
-            views.setImageViewResource(R.id.control_play, R.drawable.media_pause_dark);
+            views.setImageViewResource(R.id.control_play, R.drawable.widget_media_pause);
         } else {
-            views.setImageViewResource(R.id.control_play, R.drawable.media_start_dark);
+            views.setImageViewResource(R.id.control_play, R.drawable.widget_media_start);
         }
 
         // Set the cover art
