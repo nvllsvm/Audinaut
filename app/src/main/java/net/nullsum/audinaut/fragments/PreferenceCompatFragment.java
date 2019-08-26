nvllsvm/Audinaut

commit 66db8db769fef79016430d29d754591fa9b82609

Migrate to AppCompat

app/src/main/java/net/nullsum/audinaut/fragments/PreferenceCompatFragment.java
@@ -19,7 +19,6 @@
 
 package net.nullsum.audinaut.fragments;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -32,6 +31,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.util.Constants;
@@ -215,7 +216,7 @@ public abstract class PreferenceCompatFragment extends SubsonicFragment {
 
     private PreferenceManager createPreferenceManager() {
         try {
-            Constructor<PreferenceManager> c = PreferenceManager.class.getDeclaredConstructor(Activity.class, int.class);
+            Constructor<PreferenceManager> c = PreferenceManager.class.getDeclaredConstructor(AppCompatActivity.class, int.class);
             c.setAccessible(true);
             return c.newInstance(this.getActivity(), FIRST_REQUEST_CODE);
         } catch (Exception e) {

app/src/main/java/net/nullsum/audinaut/activity/EditPlayActionActivity.java
@@ -15,11 +15,8 @@
 
 package net.nullsum.audinaut.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -29,6 +26,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.domain.Genre;
@@ -58,7 +59,7 @@ public class EditPlayActionActivity extends SubsonicActivity {
         super.onCreate(savedInstanceState);
         setTitle(R.string.tasker_start_playing_title);
         setContentView(R.layout.edit_play_action);
-        final Activity context = this;
+        final AppCompatActivity context = this;
         doNothing = context.getResources().getString(R.string.tasker_edit_do_nothing);
 
         shuffleCheckbox = findViewById(R.id.edit_shuffle_checkbox);
@@ -218,12 +219,12 @@ public class EditPlayActionActivity extends SubsonicActivity {
 
         intent.putExtra(Constants.TASKER_EXTRA_BUNDLE, data);
 
-        setResult(Activity.RESULT_OK, intent);
+        setResult(AppCompatActivity.RESULT_OK, intent);
         finish();
     }
 
     private void cancel() {
-        setResult(Activity.RESULT_CANCELED);
+        setResult(AppCompatActivity.RESULT_CANCELED);
         finish();
     }
 }

app/src/main/java/net/nullsum/audinaut/activity/QueryReceiverActivity.java
@@ -19,10 +19,11 @@
 
 package net.nullsum.audinaut.activity;
 
-import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.util.Constants;
 import net.nullsum.audinaut.util.Util;
@@ -32,7 +33,7 @@ import net.nullsum.audinaut.util.Util;
  *
  * @author Sindre Mehus
  */
-public class QueryReceiverActivity extends Activity {
+public class QueryReceiverActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {

app/src/main/java/net/nullsum/audinaut/activity/VoiceQueryReceiverActivity.java
@@ -19,11 +19,12 @@
 
 package net.nullsum.audinaut.activity;
 
-import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.util.Constants;
 import net.nullsum.audinaut.util.Util;
@@ -35,7 +36,7 @@ import net.nullsum.audinaut.util.Util;
  *
  * @author Sindre Mehus
  */
-public class VoiceQueryReceiverActivity extends Activity {
+public class VoiceQueryReceiverActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {

app/src/main/java/net/nullsum/audinaut/fragments/SearchFragment.java
@@ -1,14 +1,16 @@
 package net.nullsum.audinaut.fragments;
 
 import android.os.Bundle;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.adapter.ArtistAdapter;
@@ -203,7 +205,7 @@ public class SearchFragment extends SubsonicFragment implements SectionAdapter.O
         task.execute();
 
         if (searchItem != null) {
-            searchItem.collapseActionView();
+            MenuItemCompat.collapseActionView(searchItem);
         }
     }
 

app/src/main/java/net/nullsum/audinaut/fragments/SubsonicFragment.java
@@ -18,7 +18,6 @@
  */
 package net.nullsum.audinaut.fragments;
 
-import android.app.Activity;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
@@ -26,13 +25,6 @@ import android.content.SharedPreferences;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.os.StatFs;
-import androidx.fragment.app.Fragment;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-import androidx.appcompat.app.AlertDialog;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.SearchView;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Menu;
@@ -45,6 +37,16 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.activity.SubsonicActivity;
@@ -151,7 +153,7 @@ public class SubsonicFragment extends Fragment implements SwipeRefreshLayout.OnR
     void onFinishSetupOptionsMenu(final Menu menu) {
         searchItem = menu.findItem(R.id.menu_global_search);
         if (searchItem != null) {
-            searchView = (SearchView) searchItem.getActionView();
+            searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
             SearchManager searchManager = (SearchManager) context.getSystemService(Context.SEARCH_SERVICE);
             SearchableInfo searchableInfo = searchManager.getSearchableInfo(context.getComponentName());
             if (searchableInfo == null) {
@@ -1444,7 +1446,7 @@ public class SubsonicFragment extends Fragment implements SwipeRefreshLayout.OnR
         boolean playNowOverride = false;
         List<Entry> songs = new ArrayList<>();
 
-        public RecursiveLoader(Activity context) {
+        public RecursiveLoader(AppCompatActivity context) {
             super(context);
             musicService = MusicServiceFactory.getMusicService(context);
         }

app/src/main/java/net/nullsum/audinaut/util/BackgroundTask.java
@@ -18,11 +18,12 @@
  */
 package net.nullsum.audinaut.util;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.view.ErrorDialog;
@@ -79,8 +80,8 @@ public abstract class BackgroundTask<T> implements ProgressListener {
         }
     }
 
-    private Activity getActivity() {
-        return (context instanceof Activity) ? ((Activity) context) : null;
+    private AppCompatActivity getActivity() {
+        return (context instanceof AppCompatActivity) ? ((AppCompatActivity) context) : null;
     }
 
     Handler getHandler() {
@@ -95,7 +96,7 @@ public abstract class BackgroundTask<T> implements ProgressListener {
 
     protected void error(Throwable error) {
         Log.w(TAG, "Got exception: " + error, error);
-        Activity activity = getActivity();
+        AppCompatActivity activity = getActivity();
         if (activity != null) {
             new ErrorDialog(activity, getErrorMessage(error), true);
         }

app/src/main/java/net/nullsum/audinaut/util/LoadingTask.java
@@ -1,7 +1,8 @@
 package net.nullsum.audinaut.util;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.activity.SubsonicActivity;
 
@@ -11,17 +12,17 @@ import net.nullsum.audinaut.activity.SubsonicActivity;
  */
 public abstract class LoadingTask<T> extends BackgroundTask<T> {
 
-    private final Activity tabActivity;
+    private final AppCompatActivity tabActivity;
     private final boolean cancellable;
     private ProgressDialog loading;
 
-    public LoadingTask(Activity activity) {
+    public LoadingTask(AppCompatActivity activity) {
         super(activity);
         tabActivity = activity;
         this.cancellable = true;
     }
 
-    public LoadingTask(Activity activity, final boolean cancellable) {
+    public LoadingTask(AppCompatActivity activity, final boolean cancellable) {
         super(activity);
         tabActivity = activity;
         this.cancellable = cancellable;

commit 3ebb48682b737d61098323745b9cd8f226544854

Fix settings Regression introduced in 66db8db769fef79016430d29d754591fa9b82609#diff-42baa789cd56fcc4c53bd06c910ff285

app/src/main/java/net/nullsum/audinaut/fragments/PreferenceCompatFragment.java
@@ -19,6 +19,7 @@
 
 package net.nullsum.audinaut.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -31,8 +32,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import net.nullsum.audinaut.R;
 import net.nullsum.audinaut.util.Constants;
@@ -216,7 +215,7 @@ public abstract class PreferenceCompatFragment extends SubsonicFragment {
 
     private PreferenceManager createPreferenceManager() {
         try {
-            Constructor<PreferenceManager> c = PreferenceManager.class.getDeclaredConstructor(AppCompatActivity.class, int.class);
+            Constructor<PreferenceManager> c = PreferenceManager.class.getDeclaredConstructor(Activity.class, int.class);
             c.setAccessible(true);
             return c.newInstance(this.getActivity(), FIRST_REQUEST_CODE);
         } catch (Exception e) {